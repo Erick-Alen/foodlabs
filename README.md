cf483c67a4145bfa94e5c62209d4401389da5488
Erick Alen Ricioli <ricioli@weg.net>
2024-01-17 16:05:01 -0300

    added Recipe view w/ 1:N relationship

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Relacionamente 1:1
-=> Pode ser configurado em qualquer entidade, desde que seja declarado `somente em uma delas`
+=> Pode ser configurado em qualquer view, desde que seja declarado `somente em uma delas`
+### Relacionamente 1:1
+N
+=> Só pode ser configurado na view responsável pelo N na relação, onde é utilizada a foreignKey `somente em uma delas`
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+    # blank is preferred for string fields
+    description = models.TextField(blank=True)
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
+    def __repr__(self) -> str:
+        return f"<[{self.id}] - {self.name}>"
